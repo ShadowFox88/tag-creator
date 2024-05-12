a98fbac1769c2ee5cf4091d21479a00ab171bd6e
cyrus01337 <cyrus21337@gmail.com>
2024-05-12 12:56:00 +0100

    Missed a comma

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-    base: "tag-creator"
+    base: "tag-creator",
     integrations: [tailwind()],
     server: {
         port: 3000,
