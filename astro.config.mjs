6179cfa2e25987a3865e78d35fa5e340dbb2c3cb
cyrus01337 <cyrus21337@gmail.com>
2024-05-12 12:55:17 +0100

    Revert to github.io domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,10 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-    base: "tag-creator",
+    base: "tag-creator"
     integrations: [tailwind()],
     server: {
         port: 3000,
     },
-    site: "https://cyrus01337.co.uk",
+    site: "https://cyrus01337.github.io",
 });
