d3ac7144534f3e371ddd4ded2f7fffd6bee60b1b
cyrus01337 <cyrus21337@gmail.com>
2024-05-12 11:50:10 +0100

    Prepare to deploy to Github Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+    base: "tag-creator",
     integrations: [tailwind()],
     server: {
         port: 3000,
     },
+    site: "https://cyrus01337.github.io",
 });
